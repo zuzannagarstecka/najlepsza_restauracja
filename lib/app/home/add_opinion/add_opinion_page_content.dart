9d8a5e84458a78ff9184c75b3ca1b2584d9cae56
Zuzanna Garstecka <garstecka.zuzanna@gmail.com>
2022-11-26 13:37:48 +0100

    Implement adding locals

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,14 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddOpinionPageContent extends StatelessWidget {
+class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
+}
+
+class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
+  var localName = '';
+  var pancakeName = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dwa'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj nazwę lokalu',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                localName = newValue;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj nazwę naleśnika',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                pancakeName = newValue;
+              });
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseFirestore.instance.collection('locals').add({
+                'name': localName,
+                'pancake': pancakeName,
+                'rating': 4.0,
+              });
+            },
+            child: const Text('Dodaj'),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,6 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:najlepsza_restauracja/app/home/add_opinion/add_opinion_page_content.dart';
+import 'package:najlepsza_restauracja/app/home/locals/locals_page_content.dart';
+import 'package:najlepsza_restauracja/app/home/my_account/my_account_page_content.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -55,92 +57,5 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
-  }
-}
-
-class MyAccountPageContent extends StatelessWidget {
-  const MyAccountPageContent({
-    Key? key,
-    required this.email,
-  }) : super(key: key);
-
-  final String? email;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Jesteś zalogowany jako $email'),
-          const SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            child: const Text('Wyloguj'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class AddOpinionPageContent extends StatelessWidget {
-  const AddOpinionPageContent({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dwa'),
-    );
-  }
-}
-
-class LocalsPageContent extends StatelessWidget {
-  const LocalsPageContent({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream: FirebaseFirestore.instance.collection('locals').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(child: Text('Something went wrong'));
-          }
-
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: Text('Loading'));
-          }
-
-          final documents = snapshot.data!.docs;
-
-          return ListView(
-            children: [
-              for (final document in documents) ...[
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(document['name']),
-                          Text(document['pancake']),
-                        ],
-                      ),
-                      Text(document['rating'].toString()),
-                    ],
-                  ),
-                ),
-              ],
-            ],
-          );
-        });
   }
 }
